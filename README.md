2e10eac742615147d543936f881000450a443e5e
Marcelo lopes da silva <Marcelo lopes da silva>
2017-06-17 02:17:50 -0300

    change readme and update unit tests

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # campanhabrinquedo_core
+
+#curl comand  
+```
+ curl -method POST -body 'username=TEST&password=TEST123' -uri http://localhost:5000/api/token
+```
+
+#up docker Data Server
+
+```
+ docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=yourStrong(!)Password' -p 1433:1433 -d microsoft/mssql-server-linux´
+```
+
+#add new reference to Project
+```
+ dotnet add [Project.csproj] reference [Project Reference.csproj]
+```
+
+#add project to solution
+```
+ dotnet sln [solutionname.sln] add [project.csproj]
+```
+
---
+++ test/campanhabrinquedo.tests/Domain/UsuarioUnitTests.cs
@@ -0,0 +1,62 @@
+namespace campanhabrinquedo.tests
+{       
+    using campanhabrinquedo.domain.Entities;
+    using Xunit;
+
+    public class UsuarioUnitTests
+    {
+        [Fact]
+        public void NovoUsuario_DadoQueDadosEstaoCorretos_DeveIntanciarNovoUsuario()
+        {
+            var usuario = new Usuario("Fulano","fulano@detal.com","123456");
+        }
+
+        [Fact]
+        public void NomeEValido_QuandoPassadoUmNomeValido_DeveRetornarVerdadeiro()
+        {
+            var usuario = new Usuario();
+            Assert.Equal(true, usuario.NomeEValido("Fulano de Tal"));
+        }
+
+        [Fact]
+        public void NomeEValido_QuandoPassadoUmNomeInvalido_DeveRetornarVerdadeiro()
+        {
+            var usuario = new Usuario();
+            Assert.Equal(false, usuario.NomeEValido(null));
+            Assert.Equal(false, usuario.NomeEValido("ABC"));
+            Assert.Equal(false, usuario.NomeEValido(""));
+        }
+
+        [Fact]
+        public void SenhaEValida_QuandoPassadoUmaSenhaInvalida_DeveRetornarVardadeiro()
+        {
+            var usuario = new Usuario();
+            Assert.Equal(true, usuario.SenhaEValida("abc123"));
+        }
+
+        [Fact]
+        public void SenhaEValida_QuandoPassadoUmaSenhaInvalida_DeveRetornarFalso()
+        {
+            var usuario = new Usuario();
+            Assert.Equal(false, usuario.SenhaEValida("abc"));
+            Assert.Equal(false, usuario.SenhaEValida(null));
+            Assert.Equal(false, usuario.SenhaEValida(""));
+        }
+
+        [Fact]
+        public void EmailEValido_QuandoPassadoUmEmailValido_DeveRetornarVerdadeiro()
+        {
+            var usuario = new Usuario();
+            Assert.Equal(false, usuario.EmailEValido("fulano@detal.com"));
+        }
+
+        [Fact]
+        public void EmailEValido_QuandoPassadoUmEmailInvalido_DeveRetornarFalso()
+        {
+            var usuario = new Usuario();
+            Assert.Equal(false, usuario.EmailEValido("fulano.com"));
+            Assert.Equal(false, usuario.EmailEValido(""));
+            Assert.Equal(false, usuario.EmailEValido(null));
+        }
+    }
+}
